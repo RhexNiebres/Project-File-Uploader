080fed6cf3db425e5200143117bfa8066352528f
RhexNiebres <rhexniebres@gmail.com>
2025-03-10 19:26:59 -0800

    Set up middleware for user access and static file handling, added routes for uploads and folders.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const session = require("express-session");
 const passport = require("./config/passport");
 const authRoutes = require("./routes/auth");
 const indexRoutes = require("./routes/index");
-
+const uploadRoutes = require("./routes/upload");
+const folderRoutes = require("./routes/folder");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -19,6 +20,16 @@ app.use(passport.session());
 
 app.use("/", indexRoutes);
 app.use("/", authRoutes);
+
+app.use((req, res, next) => {// access various local variables throughout app
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/upload", uploadRoutes);
+
+app.use("/folders", folderRoutes);
 
 
 app.listen(process.env.APP_PORT, () =>
