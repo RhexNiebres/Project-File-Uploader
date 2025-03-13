80aeaed0cdc84072b325cbcef557ea8e556406a7
RhexNiebres <rhexniebres@gmail.com>
2025-03-12 21:35:50 -0800

    Implemented Passport.js authentication with LocalStrategy for user login using Prisma and bcrypt.

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,16 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
-      const { rows } = await pool.query(
-        "SELECT * FROM users WHERE username = $1",
-        [username]
-      );
-      const user = rows[0];
+      const user = await prisma.user.findUnique({
+        where: { username: username },
+      });
+      
 
       if (!user) return done(null, false, { message: "Incorrect username" });
 
@@ -30,10 +30,9 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
-    const user = rows[0];
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+    });
     done(null, user);
   } catch (err) {
     done(err);
