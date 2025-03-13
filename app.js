da48dd7463a0c4a7f2c3886c452a53da20d1cbd6
RhexNiebres <rhexniebres@gmail.com>
2025-03-12 21:33:02 -0800

    Set up Express app with authentication, file upload routes, static file serving, and EJS view engine configuration

--- app.js
+++ app.js
@@ -7,21 +7,54 @@ const authRoutes = require("./routes/auth");
 const indexRoutes = require("./routes/index");
 const uploadRoutes = require("./routes/upload");
 const folderRoutes = require("./routes/folder");
+const fileRoutes = require("./routes/file");
 const app = express();
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// (async function() {
+//   try {
+//     // Example of uploading a single file
+//     const result = await cloudinary.uploader.upload('./uploads/sample.jpg');
+//     console.log('Upload result:', result);
+
+//     // Get the URL of the uploaded file
+//     const url = cloudinary.url(result.public_id, {
+//       transformation: [
+//         { fetch_format: 'auto' },
+//         { quality: 'auto' },
+//         { width: 1200, height: 1200 },
+//       ],
+//     });
+
+//     console.log('File URL:', url);
+//   } catch (error) {
+//     console.error('Error uploading file:', error);
+//   }
+// })();
+
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({ secret: "cats", resave: false, saveUninitialized: false })
-);
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 
 app.use("/", indexRoutes);
 app.use("/", authRoutes);
+app.use("/folders", folderRoutes);
+app.use("/file", fileRoutes);
+app.use('/files', express.static(path.join(__dirname, 'uploads')));
 
-app.use((req, res, next) => {// access various local variables throughout app
+app.use((req, res, next) => {
+  // access various local variables throughout app
   res.locals.user = req.user || null;
   next();
 });
@@ -29,7 +62,6 @@ app.use((req, res, next) => {// access various local variables throughout app
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/upload", uploadRoutes);
 
-app.use("/folders", folderRoutes);
 
 
 app.listen(process.env.APP_PORT, () =>
