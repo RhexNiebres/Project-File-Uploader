610d5c1a2188a4b583ef1bddf55efb22c157b8de
RhexNiebres <rhexniebres@gmail.com>
2025-03-12 21:40:32 -0800

    Add authentication routes for sign-up, login, and logout

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const authController = require("../controllers/authController");
+const { ensureAuthenticated } = require("../middlewares/auth");
 
 router.get("/sign-up", authController.getSignUp);
 router.post("/sign-up", authController.postSignUp);
 router.get("/login", authController.getLogin);
 router.post("/log-in", passport.authenticate("local", { successRedirect: "/", failureRedirect: "/" }));
-router.get("/log-out", authController.logout);
+router.get("/log-out", ensureAuthenticated, authController.logout);
 
 module.exports = router;
