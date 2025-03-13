dcacc43155d259daf6a6c49b2f3148720907d1d9
RhexNiebres <rhexniebres@gmail.com>
2025-03-12 21:36:38 -0800

    Implemented user login, signup, and logout with password hashing and user existence check.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 exports.getLogin = (req, res) => {
   res.render("login");
@@ -10,26 +12,26 @@ exports.getSignUp = (req, res) => {
 
 exports.postSignUp = async (req, res, next) => {
   try {
-    const existingUser = await pool.query(
-      "SELECT * FROM users WHERE username = $1",
-      [req.body.username]
-    );
+    const { firstname, lastname, username, password } = req.body;
 
-    if (existingUser.rows.length > 0) {
+    const existingUser = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (existingUser) {
       return res.status(400).send("Username already exists.");
     }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    await pool.query(
-      "INSERT INTO users (firstname, lastname, username, password, membership) VALUES ($1, $2, $3, $4)",
-      [
-        req.body.firstname,
-        req.body.lastname,
-        req.body.username,
-        hashedPassword,
-      ]
-    );
+    await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        username,
+        password: hashedPassword, 
+      },
+    });
 
     res.redirect("/");
   } catch (err) {
@@ -40,6 +42,6 @@ exports.postSignUp = async (req, res, next) => {
 exports.logout = (req, res, next) => {
   req.logout((err) => {
     if (err) return next(err);
-    res.redirect("/");
+    res.redirect("/login");
   });
 };
